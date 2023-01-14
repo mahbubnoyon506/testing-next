2abda3b7043a3acfa785036e43c3390ce76c362d
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-01-14 14:22:27 +0600

    data fetching by getstaticprops

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import User from '@/components/user';
 import Link from 'next/link';
-import React from 'react';
+import React, { user } from 'react';
 
-const Home = () => {
+const Home = ({ users }) => {
     return (
         <div>
             <div className='flex justify-center items-center py-5'>
@@ -10,8 +11,30 @@ const Home = () => {
                 <Link className='px-2 underline text-xl' href='/products'>Products</Link>
             </div>
             <h1 className=''>This is home</h1>
+            <div className=''>
+                <h2>Getting user data</h2>
+                <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-10'>
+                    {
+                        users.map((user) =>
+                            <>
+                                <User user={user} />
+                            </>
+                        )
+                    }
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    return {
+        props: {
+            users: data
+        }
+    }
+}
